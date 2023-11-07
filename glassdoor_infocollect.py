5f64586fcb78f8a6e40128196ceded42c87db082
LCode <davidzeng1828@gmail.com>
2023-11-07 21:21:06 +0800

    uploaded scrapper and run code

---
+++ glassdoor_infocollect.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 17:43:28 2023
+
+@author: User
+"""
+
+import glassdoor_scrapter as gs
+import pandas as pd
+
+
+
+df = gs.get_jobs('data scientist',1000, False, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
